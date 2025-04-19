682dc51b0cfb541195684dc11b95548e93eaa874
jscreatordev <159864313+jscreatordev@users.noreply.github.com>
2025-04-19 08:05:20 +0000

    storage fix

--- server/storage.ts
+++ server/storage.ts
@@ -48,12 +48,11 @@ export class MemStorage implements IStorage {
     this.currentCategoryId = 1;
     this.currentVideoId = 1;
 
-    // Initialize with default categories
+    // Initialize with default categories and data
     this.initializeDefaultData();
   }
 
   private initializeDefaultData() {
-    // Create default categories
     const defaultCategories: InsertCategory[] = [
       { name: "Trending", icon: "fire", slug: "trending" },
       { name: "Movies", icon: "film", slug: "movies" },
@@ -69,40 +68,76 @@ export class MemStorage implements IStorage {
       this.createCategory(category);
     });
 
-    // Create admin user with complex username
     this.createUser({
-      username: "admin_daniwdiawdnawidwandwndwadnwi",
+      username: "jscreator",
       password: "admin123",
       isAdmin: true,
     });
 
-    // Default videos list
     const sampleVideos: InsertVideo[] = [
       {
         title: "A Minecraft Movie",
         description:
           "A mysterious portal pulls four misfits into the Overworld, a bizarre, cubic wonderland that thrives on imagination. To get back home, they'll have to master the terrain while embarking on a magical quest with an unexpected crafter named Steve.",
-        categoryId: 2, // Movies category
+        categoryId: 2,
         duration: 5644,
         thumbnailUrl:
           "https://img.megaplextheatres.com/FilmBackdrop/HO00003457",
         videoUrl:
           "https://ia601602.us.archive.org/8/items/a-minecraft-movie-nova-show-01/A%20Minecraft%20Movie%20-%20NovaShow-01.mp4",
-        tags: ["minecraft", "gaming", "mojand"],
+        tags: ["minecraft", "gaming", "mojang"],
         isPopular: true,
       },
       {
-        title: "A Minecraft Movie",
-        description: "An epic adventure in a mysterious ancient city",
-        categoryId: 2, // Movies category
-        duration: 5644,
+        title: "Sonic The Hedgehog 3",
+        description:
+          "Sonic, Knuckles and Tails reunite to battle Shadow, a mysterious new enemy with powers unlike anything they've faced before. With their abilities outmatched in every way, they seek out an unlikely alliance to stop Shadow and protect the planet.",
+        categoryId: 2,
+        duration: 6617,
         thumbnailUrl:
-          "https://img.megaplextheatres.com/FilmBackdrop/HO00003457",
+          "https://i.ytimg.com/vi/qYAn4js_TsQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBWISLhBT_uZWzaWDMDCgJRl4fG1w",
         videoUrl:
-          "https://ia601602.us.archive.org/8/items/a-minecraft-movie-nova-show-01/A%20Minecraft%20Movie%20-%20NovaShow-01.mp4",
-        tags: ["minecraft", "gaming", "mojand"],
+          "https://ia601907.us.archive.org/19/items/sonic3_202504/1630858463-01.mp4",
+        tags: ["sonic", "gaming", "sega"],
         isPopular: true,
       },
+      {
+        title: "Kung fu Panda 4",
+        description:
+          "Po must train a new warrior when he's chosen to become the spiritual leader of the Valley of Peace. However, when a powerful shape-shifting sorceress sets her eyes on his Staff of Wisdom, he suddenly realizes he's going to need some help. Teaming up with a quick-witted corsac fox, Po soon discovers that heroes can be found in the most unexpected places.",
+        categoryId: 2,
+        duration: 5713,
+        thumbnailUrl:
+          "https://4kwallpapers.com/images/wallpapers/kung-fu-panda-4-1920x1080-15545.jpg",
+        videoUrl:
+          "https://ia801504.us.archive.org/9/items/kungfupanda4_202504/Watch%20Kung%20Fu%20Panda%204%202024%20Full%20HD%20Movie%20YesMovies%20to-01.mp4",
+        tags: ["dreamworks", "animated", "panda"],
+        isPopular: false,
+      },
+      {
+        title: "The Wild Robot",
+        description:
+          "After a shipwreck, an intelligent robot is stranded on an uninhabited island. To survive the harsh surroundings, she bonds with the native animals and cares for an orphaned baby goose. The film was nominated for 3 Oscars.",
+        categoryId: 2,
+        duration: 6106,
+        thumbnailUrl: "https://images3.alphacoders.com/136/1367325.jpeg",
+        videoUrl:
+          "https://ia800709.us.archive.org/15/items/wild-robot/1630858186-01.mp4",
+        tags: ["universal", "animated", "robot"],
+        isPopular: false,
+      },
+      {
+        title: "Moana 2",
+        description:
+          "After receiving an unexpected call from her wayfinding ancestors, a strong-willed girl journeys with her crew to the far seas of Oceania and into dangerous, long-lost waters for an adventure unlike anything she has ever faced.",
+        categoryId: 2,
+        duration: 5494,
+        thumbnailUrl: "https://images.squarespace-cdn.com/content/v1/5fbc4a62c2150e62cfcb09aa/1733125328711-J0YEMFJCDGLKYNC2S030/Moana%2B2%2BCollision.png",
+        videoUrl:
+          "https://ia601404.us.archive.org/14/items/moana-2_202504/Watch%20Moana%202%202024%20Full%20HD%20Movie%20YesMovies%20to.mp4",
+        tags: ["universal", "animated", "robot"],
+        isPopular: false,
+      }
     ];
 
     sampleVideos.forEach((video) => {
@@ -126,7 +161,7 @@ export class MemStorage implements IStorage {
     const user: User = {
       ...insertUser,
       id,
-      isAdmin: insertUser.isAdmin === undefined ? false : insertUser.isAdmin,
+      isAdmin: insertUser.isAdmin ?? false,
     };
     this.users.set(id, user);
     return user;
@@ -193,10 +228,7 @@ export class MemStorage implements IStorage {
   }
 
   async deleteVideo(id: number): Promise<boolean> {
-    if (this.videos.has(id)) {
-      return this.videos.delete(id);
-    }
-    return false;
+    return this.videos.delete(id);
   }
 
   async updateVideoViews(id: number): Promise<Video | undefined> {
